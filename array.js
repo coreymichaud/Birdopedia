cad1b19c1ba508cb8e2c9a52409105730b71de77
Corey M <63071835+coreymichaud@users.noreply.github.com>
2021-11-13 13:26:41 -0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,18 @@
+var randBird = [
+
+    'https://en.wikipedia.org/wiki/Mallard', 'https://en.wikipedia.org/wiki/Domestic_duck', 'https://en.wikipedia.org/wiki/Canvasback', 'https://en.wikipedia.org/wiki/Muscovy_duck', 
+    'https://en.wikipedia.org/wiki/Black-bellied_whistling_duck', 'https://en.wikipedia.org/wiki/Mandarin_duck', 'https://en.wikipedia.org/wiki/West_Indian_whistling_duck', 
+    'https://en.wikipedia.org/wiki/Canada_goose', 'https://en.wikipedia.org/wiki/Northern_pintail', 'https://en.wikipedia.org/wiki/Gadwall', 'https://en.wikipedia.org/wiki/Wood_duck', 
+    'https://en.wikipedia.org/wiki/Redhead_(bird)', 'https://en.wikipedia.org/wiki/Bufflehead', 'https://en.wikipedia.org/wiki/King_eider', 'https://en.wikipedia.org/wiki/Common_goldeneye', 
+    'https://en.wikipedia.org/wiki/Hooded_merganser', 'https://en.wikipedia.org/wiki/Snow_goose', 'https://en.wikipedia.org/wiki/Emperor_goose', 'https://en.wikipedia.org/wiki/Cackling_goose', 
+    'https://en.wikipedia.org/wiki/American_coot', 'https://en.wikipedia.org/wiki/Mute_swan', 'https://en.wikipedia.org/wiki/American_wigeon', 'https://en.wikipedia.org/wiki/American_black_duck', 
+    'https://en.wikipedia.org/wiki/Eurasian_wigeon', 'https://en.wikipedia.org/wiki/Northern_pintail', 'https://en.wikipedia.org/wiki/Green-winged_teal', 'https://en.wikipedia.org/wiki/Mottled_duck', 
+    'https://en.wikipedia.org/wiki/Black_scoter', 'https://en.wikipedia.org/wiki/Common_merganser', 'https://en.wikipedia.org/wiki/Barrow%27s_goldeneye', 'https://en.wikipedia.org/wiki/Long-tailed_duck', 
+    'https://en.wikipedia.org/wiki/Ring-necked_duck', 'https://en.wikipedia.org/wiki/Surf_scoter', 'https://en.wikipedia.org/wiki/Spectacled_eider', 'https://en.wikipedia.org/wiki/Steller%27s_eider', 
+    'https://en.wikipedia.org/wiki/Fulvous_whistling_duck', 'https://en.wikipedia.org/wiki/Sandhill_crane', 'https://en.wikipedia.org/wiki/Great_blue_heron', 'https://en.wikipedia.org/wiki/Grey_heron', 
+    'https://en.wikipedia.org/wiki/Little_egret', 'https://en.wikipedia.org/wiki/Great_egret', 'https://en.wikipedia.org/wiki/Anhinga', 'https://en.wikipedia.org/wiki/Darter', 
+    'https://en.wikipedia.org/wiki/Cormorant', 'https://en.wikipedia.org/wiki/Great_cormorant', 'https://en.wikipedia.org/wiki/Ibis', 'https://en.wikipedia.org/wiki/Sanderling', 
+    'https://en.wikipedia.org/wiki/Grebe', 'https://en.wikipedia.org/wiki/Gull', 'https://en.wikipedia.org/wiki/Pelican'
+
+
+]
